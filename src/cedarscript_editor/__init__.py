0cfa8e9604f74d697f79c359a5484b3543c0934a
Elifarley <elifarley@gmail.com>
2024-10-17 00:03:22 -0300

    Use setuptools_scm

--- src/cedarscript_editor/__init__.py
+++ src/cedarscript_editor/__init__.py
@@ -1,6 +1,12 @@
-from cedarscript_editor.cedarscript_editor import CEDARScriptEditor
+from ._version import __version__
+import re
+from .cedarscript_editor import CEDARScriptEditor
+from text_manipulation import IndentationInfo, IdentifierBoundaries, RangeSpec, read_file, write_file, bow_to_search_range
 
-__version__ = "0.2.3"
+__all__ = [
+    "__version__", "find_commands", "CEDARScriptEditor", "IndentationInfo", "IdentifierBoundaries", "RangeSpec",
+    "read_file", "write_file", "bow_to_search_range"
+]
 
 __all__ = ["CEDARScriptEditor"]
 
